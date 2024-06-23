76570e6072b7e3ffbcfc185b297b622eea6806ce
JanetAdedosu <adedosuadeola71@gmail.com>
2024-06-23 20:26:45 +0200

    new update

--- entities/index.js
+++ entities/index.js
@@ -1,8 +1,9 @@
 import Matter from "matter-js"
 import Bird from "../components/Bird";
 import Floor from "../components/Floor";
-import { Dimensions } from 'react-native'
 import Obstacle from "../components/Obstacle";
+
+import { Dimensions } from 'react-native'
 import { getPipeSizePosPair } from "../utils/random";
 
 const windowHeight = Dimensions.get('window').height
@@ -13,11 +14,11 @@ export default restart => {
     let engine = Matter.Engine.create({ enableSleeping: false })
 
     let world = engine.world
+
     world.gravity.y = 0.4;
 
     const pipeSizePosA = getPipeSizePosPair()
-    
-
+    const pipeSizePosB = getPipeSizePosPair(windowWidth * 0.9)
     return {
         physics: { engine, world },
 
@@ -25,7 +26,10 @@ export default restart => {
 
         ObstacleTop1: Obstacle(world, 'ObstacleTop1', 'red', pipeSizePosA.pipeTop.pos, pipeSizePosA.pipeTop.size),
         ObstacleBottom1: Obstacle(world, 'ObstacleBottom1', 'blue', pipeSizePosA.pipeBottom.pos, pipeSizePosA.pipeBottom.size),
-        
-        Floor: Floor(world, 'green',{ x: windowWidth / 2, y: windowHeight }, { height: 50, width: windowWidth } ),
+
+        ObstacleTop2: Obstacle(world, 'ObstacleTop2', 'red', pipeSizePosB.pipeTop.pos, pipeSizePosB.pipeTop.size),
+        ObstacleBottom2: Obstacle(world, 'ObstacleBottom2', 'blue', pipeSizePosB.pipeBottom.pos, pipeSizePosB.pipeBottom.size),
+
+        Floor: Floor(world, 'green', { x: windowWidth / 2, y: windowHeight }, { height: 50, width: windowWidth })
     }
 }
